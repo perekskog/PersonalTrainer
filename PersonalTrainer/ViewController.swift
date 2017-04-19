068b697a3ff26132f3a46c367db562b012ae15d6
Per Ekskog <per.ekskog@gmail.com>
2017-04-19 22:36:48 +0200

    Start/stop buttons, vibrate every 3 seconds.

--- PersonalTrainer/ViewController.swift
+++ PersonalTrainer/ViewController.swift
@@ -11,11 +11,15 @@ import AudioToolbox
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var vibrateButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
     
+    var vibrateTimer: Timer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,10 +27,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func start(_ sender: UIButton) {
+        vibrateTimer = Timer.scheduledTimer(timeInterval: 3,
+                                            target: self,
+                                            selector: #selector(ViewController.vibrate(_:)),
+                                            userInfo: nil,
+                                            repeats: true)
+    }
 
-    @IBAction func vibrate(_ sender: UIButton) {
-        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+    @IBAction func stop(_ sender: UIButton) {
+        vibrateTimer?.invalidate()
     }
     
+    func vibrate(_ timer: Timer) {
+        print("vibrate")
+        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+    }
 }
 
